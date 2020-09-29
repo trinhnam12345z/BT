d0b43533b515c20f002bdce6eb3a4eea322518f6
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-29 10:13:12 +0700

    bt

---
+++ Đoc ghi file XML/Đoc ghi file XML/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Đoc_ghi_file_XML
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public float Price { get; set; }
+    }
+}
